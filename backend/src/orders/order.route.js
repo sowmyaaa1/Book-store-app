6dc05dcf52eadda457ffcc02c0c7c7048153d6d9
sowmyaaa1 <91062528+sowmyaaa1@users.noreply.github.com>
2025-03-05 12:43:43 +0530

    Update order.route.js

--- backend/src/orders/order.route.js
+++ backend/src/orders/order.route.js
@@ -6,7 +6,7 @@ const router =  express.Router();
 // create order endpoint
 router.post("/", createAOrder);
 
-// get orders by user email 
+
 router.get("/email/:email", getOrderByEmail);
 
 module.exports = router;
