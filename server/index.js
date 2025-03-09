459eb127dfa8422c645cac491e0edf21e5566335
sowmyaaa1 <sowmya.24it@licet.ac.in>
2025-03-09 10:58:28 +0530

    Changes made to build success

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://book-app-frontend-tau.vercel.app'],
+    origin: ['http://localhost:5173', 'book-store-backend-d4cce8dfgkf4erdq.canadacentral-01.azurewebsites.net'],
     credentials: true
 }))
 
